6cc9d0a26e668e69ef7c360a0039e0919e28c7fb
Mohammed Afsal <mohammedafsaltut@gmail.com>
2025-01-02 15:43:41 +0530

    Create Header UI

--- src/app/_components/header/Header.js
+++ src/app/_components/header/Header.js
@@ -2,22 +2,35 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-black text-white h-20 flex items-center justify-between px-24">
+    <header className="bg-white text-gray-700 h-14 flex items-center justify-between px-6 md:px-24">
       <nav className="flex items-center justify-between w-full">
-        <h1 className="text-lg font-bold">WorkFlow</h1>
-        <div className="space-x-4 flex items-center">
+        <h1 className="text-slate-800 text-3xl font-bold">WorkFlow</h1>
+
+        <div className="flex space-x-8 mx-auto">
           <Link href="/">Home</Link>
           <Link href="/work-management">Work Management</Link>
           <Link href="/dev">Dev</Link>
           <Link href="/crm">CRM</Link>
           <Link href="/service">Service</Link>
         </div>
-        <Link
-          className="bg-blue-600 py-1 px-3 rounded-md hover:bg-blue-700"
-          href="#"
-        >
-          Logout
-        </Link>
+
+        <div className="space-x-4 flex items-center">
+          <Link
+            className="py-2 px-6 rounded-full border border-violet-400"
+            href="/auth/signin"
+          >
+            Login
+          </Link>
+          <Link
+            href="/auth/signin"
+            className="relative py-2 px-6 rounded-full border border-violet-400 text-violet-400 overflow-hidden group"
+          >
+            <span className="absolute inset-0 w-full h-full bg-violet-400 transform scale-x-0 origin-right group-hover:scale-x-100 transition-transform duration-300 rounded-full"></span>
+            <span className="relative z-10 group-hover:text-white transition-colors duration-300">
+              Get Started
+            </span>
+          </Link>
+        </div>
       </nav>
     </header>
   );
